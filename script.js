773e18ba81dde6981996d381c4f0725273408654
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-22 10:19:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+function daysOfAYear(year){
+        if ((year % 4==0 && year % 100 !=0 )||(year % 400===0)){
+			return 365;
+		}
+	else{
+         return 366;
+	}
+}
+console.log(daysOfAYear(2022));
+console.log(daysOfAYear(2024));
+console.log(daysOfAYear(1900));
+console.log(daysOfAYear(2000));
+
